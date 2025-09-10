f8df4bc0ef3fe8d50ff99fddf4f21df169e56d6a
edigasunil <sunilediga679@gmail.com>
2025-09-11 01:12:25 +0530

    Merged backend subfolders into backend/ directory

--- routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCart, addToCart } = require("../controllers/cartController");
-
-router.get("/", getCart);
-router.post("/", addToCart);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signup, login } = require("../controllers/userController");
-
-router.get("/", (req, res) => res.send("User route works"));
-router.post("/signup", signup);
-router.post("/login", login);
-
-module.exports = router;
